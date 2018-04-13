f7d43d4b17222e707d43d3d7be2026750c2ead34
Krzysztof Gonciarz <gonciarz@mpi-cbg.de>
2018-04-13 12:32:59 +0200

    Added submodules and initial main file using APR

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+#include "data_structures/Mesh/MeshData.hpp"
+#include "algorithm/LocalIntensityScale.hpp"
+#include "algorithm/LocalIntensityScaleCuda.h"
+#include "TestTools.hpp"
+
+namespace {
+    TEST(LocalIntensityScaleTest, 1D_Y_DIR) {
+        {   // OFFSET=0
+
+            MeshData<float> m(8, 1, 1, 0);
+            float dataIn[] = {3,6,9,12,15,18,21,24};
+            float expect[] = {3,6,9,12,15,18,21,24};
+
+            initFromZYXarray(m, dataIn);
+
+            LocalIntensityScale lis;
+            lis.calc_sat_mean_y(m, 0);
+
+            ASSERT_TRUE(compare(m, expect, 0.05));
+        }
+	}
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
