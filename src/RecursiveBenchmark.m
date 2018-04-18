f1546d3ca3b78cde12d0357e4454ad6765c4c719
Krzysztof Gonciarz <gonciarz@mpi-cbg.de>
2018-04-18 19:59:32 +0200

    Added init matlab files

---
+++ src/RecursiveBenchmark.m
@@ -0,0 +1,72 @@
+function RecursiveBenchmark()
+addpath('/Users/gonciarz/Documents/MOSAIC/work/repo/RecursiveGPU/APRBench/Matlab');
+
+% name='asdf.h5';
+analysis_root='/Users/gonciarz/Documents/MOSAIC/work/repo/RecursiveGPU/build/';
+
+figure(1);
+clf;
+hold on;
+plotData([analysis_root, 'asdf.h5'], 1);
+% plotData([analysis_root, 'asdfOld.h5'], 1);
+
+function plotData(fileName, plotNum)
+    ad = load_analysis_data(fileName);
+
+    % Test options
+    numOfRep = ad.numOfRepetitions;
+    skipNumOfFirstElements=ad.numOfRepetitionsToSkip;
+    
+    [cpuData, cpuErr]=getMeanMeasurements(ad.CpuTime, numOfRep, skipNumOfFirstElements);
+    [gpuData, gpuErr]=getMeanMeasurements(ad.GpuDeviceTimeFull, numOfRep, skipNumOfFirstElements);
+
+    x=ad.ticksValue;
+    
+    figure(plotNum);
+    format_figure(gcf);
+    hold on;
+    cm_type = 'parula(5)';
+    cm = colormap(cm_type)
+    errorbar(x, cpuData,cpuErr, 'color', cm(3,:));
+    errorbar(x, gpuData,gpuErr, 'color', cm(1,:));
+    
+%     axis([0 Inf 0 Inf]);    
+    set(gca,'XTick', ad.ticksValue)
+    set(gca,'XTickLabel', num2str(ad.ticksValue/ad.xNormalizer,strcat('%.',num2str(ad.numberOfDecimalPointsX),'f')));
+    l = legend({'CPU', 'GPU'});
+    l.Location = 'best';
+    l.Box = 'off';
+    l.FontSize = 20;
+   
+    xlabel(ad.xTitle');
+    ylabel(ad.yTitle');            
+    
+    title(ad.plotTitle');
+    
+end
+
+function [out, maxErr]=getMeanMeasurements(data, noOfRep, skipSteps)
+    numOfOutElements = length(data) / noOfRep;
+    out = zeros(numOfOutElements,1);
+    maxErr = zeros(numOfOutElements,1);
+    i = 1;
+    for idx=1:noOfRep:length(data)-(noOfRep-1)
+        out(i) = mean(data(idx + skipSteps : idx + noOfRep - 1));
+        maxErr(i) = std(data(idx + skipSteps:idx+noOfRep - 1));
+        i = i + 1;
+    end
+end
+
+function [out, maxErr]=getMinMeasurements(data, noOfRep, skipSteps)
+    numOfOutElements = length(data) / noOfRep;
+    out = zeros(numOfOutElements,1);
+    maxErr = zeros(numOfOutElements,1);
+    i = 1;
+    for idx=1:noOfRep:length(data)-(noOfRep-1)
+        out(i) = min(data(idx + skipSteps : idx + noOfRep - 1));
+        maxErr(i) = std(data(idx + skipSteps:idx+noOfRep - 1));
+        i = i + 1;
+    end
+end
+
+end
--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,77 @@
-#include <gtest/gtest.h>
-#include "data_structures/Mesh/MeshData.hpp"
-#include "algorithm/LocalIntensityScale.hpp"
-#include "algorithm/LocalIntensityScaleCuda.h"
+#include "misc/APRTimer.hpp"
 #include "AnalysisData.hpp"
+
+#include <gtest/gtest.h>
 #include "TestTools.hpp"
+
+#include "data_structures/Mesh/MeshData.hpp"
 #include "algorithm/ComputeGradient.hpp"
 #include "algorithm/ComputeBsplineRecursiveFilterCuda.h"
 
+AnalysisData ad;
+
 namespace {
-    TEST(LocalIntensityScaleTest, 1D_Y_DIR) {
-        {   // OFFSET=0
+    TEST(BenchmarkBsplineTest, FULL) {
+        ad.file_name = "asdf";
+
+        using ImgType = float;
+        const int numOfRepetitions = 5; // how many runs per each mesh size should be done
+        const int numOfRepetitionsToSkip = 1; // how many first runs should be skipped from plot generation
+        ad.add_float_data("numOfRepetitionsToSkip", numOfRepetitionsToSkip);
+        ad.add_float_data("numOfRepetitions", numOfRepetitions);
+
+        ad.add_string_data("xTitle", "Mesh size in GB");
+        ad.add_string_data("yTitle", "Time in seconds");
+        ad.add_float_data("xNormalizer", 1024*1024*1024);
+        ad.add_float_data("numberOfDecimalPointsX", 1);
+        ad.add_string_data("plotTitle", "BSpline recursive filter TitanX vs 10xXenon 2.4GHz");
+        // Filter parameters
+        const float lambda = 3;
+        const float tolerance = 0.0001;
+
+        { // must be in scope of block to destroy APRTimer (destructors writes data to AnalysisData)
+
             APRTimer timer(true);
-            const char *name = "cmdName";
-            AnalysisData ad("This is name", "And this is description", 1, &name);
-            ad.name = "asdf";
 
-            using ImgType = float;
-            MeshData<ImgType> m = getRandInitializedMesh<ImgType>(512,256,256);
+            int numberOfTests = 7 + 1;
+            for (size_t d = 0; d < numberOfTests; ++d) {
+                size_t yLen = 1024;
+                size_t zLen = 1024;
+                size_t xStartValue = 256;
+                size_t deltaStep = 256;
+                size_t xLen = xStartValue + deltaStep * d;
 
-            // Filter parameters
-            const float lambda = 3;
-            const float tolerance = 0.0001;
+                std::cout << "========================= " << d+1 << "/" << numberOfTests << " ===============\n";
+                ad.add_float_data("ticksValue", xLen * zLen * yLen * sizeof(ImgType));
+                MeshData<ImgType> m = getRandInitializedMesh<ImgType>(yLen, xLen, zLen);
+                std::cout << "MESH: " << m << std::endl;
+                for (int i = 0; i < numOfRepetitions; ++i) {
+                    std::cout << "<<<<<<<<<<<<<<<<<<< REPETITION CPU: " << i + 1 << "/" << numOfRepetitions << "\n";
+                    // Calculate bspline on CPU
+                    ComputeGradient cg;
+                    timer.start_timer("CpuTime");
+                    MeshData<ImgType> mCpu(m, true);
+                    cg.bspline_filt_rec_y(mCpu, lambda, tolerance);
+                    cg.bspline_filt_rec_x(mCpu, lambda, tolerance);
+                    cg.bspline_filt_rec_z(mCpu, lambda, tolerance);
+                    timer.stop_timer();
+                }
 
-            for (int i = 0; i < 10; ++i) {
+                for (int i = 0; i < numOfRepetitions; ++i) {
+                    std::cout << "<<<<<<<<<<<<<<<<<<< REPETITION GPU: " << i + 1 << "/" << numOfRepetitions << "\n";
 
-                // Calculate bspline on CPU
-                ComputeGradient cg;
-                MeshData<ImgType> mCpu(m, true);
-                timer.start_timer("CPU bspline");
-                cg.bspline_filt_rec_y(mCpu, lambda, tolerance);
-                cg.bspline_filt_rec_x(mCpu, lambda, tolerance);
-                cg.bspline_filt_rec_z(mCpu, lambda, tolerance);
-                timer.stop_timer();
+                    // Calculate bspline on GPU
+                    MeshData<ImgType> mGpu(m, true);
+                    timer.start_timer("GpuTimeWithMemTransfer");
+                    cudaFilterBsplineFull(mGpu, lambda, tolerance, BSPLINE_ALL_DIR);
+                    timer.stop_timer();
+                }
 
-                // Calculate bspline on GPU
-                MeshData<ImgType> mGpu(m, true);
-                timer.start_timer("GPU bspline");
-                cudaFilterBsplineFull(mGpu, lambda, tolerance, BSPLINE_ALL_DIR);
-                timer.stop_timer();
-
-                EXPECT_EQ(compareMeshes(mCpu, mGpu), 0);
             }
-
-            ad.add_timer(timer);
-            ad.write_analysis_data_hdf5();
         }
-	}
+
+        ad.write_analysis_data_hdf5();
+    }
 }
 
 int main(int argc, char **argv) {
