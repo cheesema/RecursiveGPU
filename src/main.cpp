61b3a0abeb32f825309acf4e2389d7b575ce879a
Krzysztof Gonciarz <gonciarz@mpi-cbg.de>
2018-04-19 10:53:43 +0200

    Warnings removed

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,10 @@
 #include "algorithm/ComputeGradient.hpp"
 #include "algorithm/ComputeBsplineRecursiveFilterCuda.h"
 
+#ifdef HAVE_OPENMP
+#include <omp.h>
+#endif
+
 AnalysisData ad;
 
 namespace {
@@ -24,7 +28,7 @@ namespace {
         ad.add_string_data("yTitle", "Time in seconds");
         ad.add_float_data("xNormalizer", 1024*1024*1024);
         ad.add_float_data("numberOfDecimalPointsX", 1);
-        ad.add_string_data("plotTitle", "BSpline recursive filter TitanX vs 10xXenon 2.4GHz");
+        ad.add_string_data("plotTitle", "recursive filter Titan X vs 10 x Xeon(R) @2.60GHz");
         // Filter parameters
         const float lambda = 3;
         const float tolerance = 0.0001;
@@ -32,16 +36,21 @@ namespace {
         { // must be in scope of block to destroy APRTimer (destructors writes data to AnalysisData)
 
             APRTimer timer(true);
+	    #ifdef HAVE_OPENMP
 
-            int numberOfTests = 7 + 1;
+	    int numOfThreads = omp_get_num_procs();
+	    std::cout << "OpenMP reports " << numOfThreads << " threads available." << std::endl;
+	    #endif
+
+            unsigned int numberOfTests = 15 + 1;
             for (size_t d = 0; d < numberOfTests; ++d) {
                 size_t yLen = 1024;
                 size_t zLen = 1024;
-                size_t xStartValue = 256;
-                size_t deltaStep = 256;
+                size_t xStartValue = 128;
+                size_t deltaStep = 128;
                 size_t xLen = xStartValue + deltaStep * d;
 
-                std::cout << "========================= " << d+1 << "/" << numberOfTests << " ===============\n";
+                std::cout << "\n\n========================= " << d+1 << "/" << numberOfTests << " ===============\n";
                 ad.add_float_data("ticksValue", xLen * zLen * yLen * sizeof(ImgType));
                 MeshData<ImgType> m = getRandInitializedMesh<ImgType>(yLen, xLen, zLen);
                 std::cout << "MESH: " << m << std::endl;
